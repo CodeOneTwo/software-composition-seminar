fbb89f2c26be7da9c98f8ed046548c9a3b9ff6e0
Jan Dietrich <jan.dietrich@tecpose.com>
2020-12-01 01:00:15 +0100

    added style of slides

--- presentation/page-number.js
+++ presentation/page-number.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useDeck } from "mdx-deck"; // Import useDeck
 
-const PageNumber = ({ children }) => {
+const PageNumber = () => {
   const state = useDeck(); // Declare a new state variable
 
   const currentSlide = state.index + 1; // The slides are zero-index
   return (
-      <p style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{currentSlide}</p>
+      <p style={{ fontWeight: 'bold', fontSize: '1.8em' }}>{currentSlide}</p>
   );
 };
 
